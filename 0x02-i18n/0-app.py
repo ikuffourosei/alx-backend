1f25b6a6dd95e214e1e5280cc2df62b8b29a6c97
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-02 16:50:04 +0000

    flask babel

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,15 +2,18 @@
 """A simple flask app"""
 
 from flask import Flask, render_template
+from flask_cors import CORS
 
 
 myapp = Flask(__name__)
+cors = CORS(myapp, resources={r"/*": {"origin": "*"}})
 
 
 @myapp.route('/', methods=['GET'], strict_slashes=False)
 def view_page():
     """Displays the home page"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     myapp.run()
