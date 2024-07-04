565874a8ede7e2d08bcb8f777eb884124af33ea0
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-04 12:41:41 +0000

    flask babel

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+'''A simple flask app to translate languages'''
+
+from flask import Flask, g, render_template, request
+from flask_babel import Babel
+from flask_cors import CORS
+
+
+myapp = Flask(__name__)
+cors = CORS(myapp, resources={r"/*": {'origins': "*"}})
+babel = Babel(myapp)
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "fr"
+
+
+myapp.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Gets the best language match from user request"""
+    if request.args.get('locale') is not None:
+        lang = request.args.get('locale')
+        if lang in myapp.config['LANGUAGES']:
+            return lang
+    else:
+        return request.accept_languages.best_match(myapp.config['LANGUAGES'])
+
+@myapp.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """returns the home page"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    myapp.run()
